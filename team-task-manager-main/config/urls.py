7fcdc7b34d49ce3ee85ceea63fa3957ec200b8a9
Prince2202031800047 <pckavathiya1@gmail.com>
2026-05-04 17:22:56 +0530

    fix login route

--- team-task-manager-main/config/urls.py
+++ team-task-manager-main/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import login_page
-from core.views import  dashboard, signup_page
+from core.views import  dashboard, signup_page,login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,7 +9,7 @@ urlpatterns = [
 
     path('', login_page),
     path('dashboard/', dashboard),
-    path('signup/', signup_page)
+    path('signup/', signup_page),
     path('login/', login_view),
     
 
